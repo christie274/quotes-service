e2e68d70d31715b9fe7a542d5b9879f335aeb56e
Christie Ryan <cryan11@cnm.edu>
2020-07-13 11:59:27 -0600

    Continuing OAuth 2.0 integration (almost complete)

--- src/main/java/edu/cnm/deepdive/quotes/service/GoogleTokenService.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/GoogleTokenService.java
@@ -1,21 +1,27 @@
 package edu.cnm.deepdive.quotes.service;
 
-
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import edu.cnm.deepdive.quotes.model.entity.User;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collection;
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
@@ -30,11 +36,12 @@ public class GoogleTokenService implements ResourceServerTokenServices {
 
   private final String clientId;
   private final AccessTokenConverter converter;
-  //TODO Reference UserService.
+  private final UserService userService;
 
   @Autowired
-  public GoogleTokenService(@Value("${oauth.clientId}") String clientId) {
+  public GoogleTokenService(@Value("${oauth.clientId}") String clientId, UserService userService) {
     this.clientId = clientId;
+    this.userService = userService;
     converter = new DefaultAccessTokenConverter();
   }
 
@@ -43,8 +50,8 @@ public class GoogleTokenService implements ResourceServerTokenServices {
   public OAuth2Authentication loadAuthentication(String token)
       throws AuthenticationException, InvalidTokenException {
     HttpTransport transport = new NetHttpTransport();
-    JacksonFactory jacksonFactory = new JacksonFactory();
-    GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jacksonFactory)
+    JacksonFactory jsonFactory = new JacksonFactory();
+    GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
         .setAudience(Collections.singletonList(clientId))
         .build();
     GoogleIdToken idToken = null;
@@ -57,17 +64,21 @@ public class GoogleTokenService implements ResourceServerTokenServices {
       Payload payload = idToken.getPayload();
       String oauthKey = payload.getSubject();
       String displayName = (String) payload.get("name");
-      //TODO Extract and additional required info from payload.
-      //TODO Request user from UserService.
-      return null; // Replace with actual value.
+      // TODO Extract any additional required info from payload.
+      User user = userService.readOrCreateOne(oauthKey, displayName);
+      Collection<GrantedAuthority> grants = Collections.singleton(
+          new SimpleGrantedAuthority(String.format(ROLE_PATTERN, user.getRole())));
+      Authentication base = new UsernamePasswordAuthenticationToken(user, token, grants);
+      OAuth2Request request = converter.extractAuthentication(payload).getOAuth2Request();
+      return new OAuth2Authentication(request, base);
     } else {
       throw new InvalidTokenException(VERIFICATION_FAILURE);
     }
-
   }
 
   @Override
   public OAuth2AccessToken readAccessToken(String s) {
     return null;
   }
+
 }
